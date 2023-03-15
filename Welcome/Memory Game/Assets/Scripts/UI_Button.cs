71b4d82468ca57318e116d4e80ebf0055ad9ed46
Alex D <amdaley@udel.edu>
2023-03-15 04:33:05 -0400

    Memory Game

---
+++ Welcome/Memory Game/Assets/Scripts/UI_Button.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Button : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+
+    public Color highlightColor = Color.red;
+
+    public void OnMouseOver(){
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite != null){
+            sprite.color = highlightColor;
+        }
+    }
+
+    public void OnMouseExit(){
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if(sprite != null){
+            sprite.color = Color.white;
+        }
+    }
+    public void OnMouseDown(){
+        transform.localScale = new Vector3(1.1f,1.1f,1.1f);
+
+    }
+    public void OnMouseUp(){
+        transform.localScale = Vector3.one;
+        if(targetObject != null){
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
